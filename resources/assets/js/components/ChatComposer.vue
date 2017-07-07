4b3105f4658e0b4926f2894760886eac8f21dd90
drsdemesa <macbookpro@MacbookPros-MacBook-Pro.local>
2017-07-07 22:08:31 +0800

    shortcut for v-on

--- resources/assets/js/components/ChatComposer.vue
+++ resources/assets/js/components/ChatComposer.vue
@@ -1,7 +1,7 @@
 <template lang="html">
 	<div class="chat-composer">
 		<input type="text" placeholder="Start typing your message here..." v-model="messageText"
-			v-on:keydown.enter="sendMessage">
+			@keydown.enter="sendMessage">
 		<button class="btn btn-primary" @click="sendMessage">Send</button>
 	</div>
 </template>
