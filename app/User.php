94739c91dd1eddd84486a1162ea45e5baa0da6a4
drsdemesa <macbookpro@MacbookPros-MacBook-Pro.local>
2017-07-08 15:08:18 +0800

    add relationship to message and user

--- app/User.php
+++ app/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    /*
+     * associate to messages
+     */
+    public function messages(){
+        return $this->hasMany(Message::class);
+    }
 }
