6cbcb4310f0d5df34de0ce8cf786cfa7c4877c21
drsdemesa <macbookpro@MacbookPros-MacBook-Pro.local>
2017-07-09 19:35:29 +0800

    created .env.dusk.local to use sqlite db for dusk testing, and run this with php artisan serve --env=dusk.local

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -17,24 +17,34 @@ class ExampleTest extends DuskTestCase
      */
     public function testBasicExample()
     {
+        // check if home is working
         // $this->browse(function (Browser $browser) {
         //     $browser->visit('/')
         //             ->assertSee('Laravel');
         // });
 
-        $this->browse(function ($first, $second) {
-            $first->loginAs(User::find(1))
+        //create dummy users 1 and 2
+        $user1 = factory(User::class)->create([
+                'name' => 'Jane Day'
+            ]);
+        $user2 = factory(User::class)->create([
+                'name' => 'Jana Smith'
+            ]);
+
+        //login as user #1 and #2 and send dummy chat
+        $this->browse(function ($first, $second) use ($user1, $user2) {
+            $first->loginAs($user1)
                   ->visit('/chat')
                   ->waitFor('.chat-composer');
 
-            $second->loginAs(User::find(2))
+            $second->loginAs($user2)
                    ->visit('/chat')
                    ->waitFor('.chat-composer')
-                   ->type('message', 'Hey Taylor')
+                   ->type('message', 'Hey Jane')
                    ->press('Send');
 
-            $first->waitForText('Hey Taylor')
-                  ->assertSee('Uchi');
+            $first->waitForText('Hey Jane')
+                  ->assertSee('Jana Smith');
         });
     }
 }
